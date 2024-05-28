540bde1ed4d46bfb058798271ca5eba03c2f3d01
Hanma-yujiro <mpethamotaung@gmail.com>
2024-05-28 21:14:12 +0200

    create homepage app

--- bright_project/homepage/apps.py
+++ bright_project/homepage/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BrightAppConfig(AppConfig):
+class HomepageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "bright_app"
+    name = "homepage"
