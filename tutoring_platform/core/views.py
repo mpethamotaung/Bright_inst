778ed49b9a617df9fc4eb128003d76298f675782
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-03 10:15:09 +0200

    create views for core app

--- tutoring_platform/core/views.py
+++ tutoring_platform/core/views.py
@@ -4,3 +4,14 @@ from rest_framework import viewsets
 from .models import User, Subject, Session
 from .serializers import UserSerializer, SubjectSerializer, SessionSerializer
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class SessionViewSet(viewsets.ModelViewSet):
+    queryset = Session.objects.all()
+    serailizer_class = SessionSerializer
