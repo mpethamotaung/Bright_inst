06f341c608d890417ec1080e7fb95f75990b9299
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-09 12:01:44 +0200

    create serializers.py, imports, and create class | create views.py, imports and create class

--- tutoring_platform/authentication/users/views.py
+++ tutoring_platform/authentication/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
+
+class UserCreate(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
 # Create your views here.
