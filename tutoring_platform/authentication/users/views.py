68dc8d89e081217e4610c5b97c0c649c6f1777e3
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 08:21:16 +0200

    in authentication/users mod views.py, with a class that defines a view for user registration using django_restframework

--- tutoring_platform/authentication/users/views.py
+++ tutoring_platform/authentication/users/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-
 from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 
+#initiate user class to allow users to access this endpoint
 class UserCreate(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-
-
-# Create your views here.
