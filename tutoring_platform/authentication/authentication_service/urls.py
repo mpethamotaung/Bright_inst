1688d83a43e514fd45c54d7fb4175e84de3fd464
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 08:24:35 +0200

    connects main authentication_service urls to user app urls (include)

--- tutoring_platform/authentication/authentication_service/urls.py
+++ tutoring_platform/authentication/authentication_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/users/', include('users.url')),
+    path('api/users/', include('users.url')), #connects main authentication_service urls to user app urls
 ]
