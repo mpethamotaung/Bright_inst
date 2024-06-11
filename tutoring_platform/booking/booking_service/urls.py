fc176cabdea2f980ebf8aa6d8e7a2bcaa9b66c29
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 17:54:36 +0200

    booking/booking_service/urls.py include appointment URLs to make them accessible to clients

--- tutoring_platform/booking/booking_service/urls.py
+++ tutoring_platform/booking/booking_service/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/', include('appointments.urls')),
 ]
+
+"""include the appointments URLs under the '/api/' path, making them
+    accessible to clients"""
