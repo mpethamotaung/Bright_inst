29d48654a258681c7a8c67dcac07125359494609
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 19:19:46 +0200

    tutoring_platform/courses/course/view.py theses classes defined are DRF generic views to handling listing, creating, retrieving, updating, and deleting courses

--- tutoring_platform/courses/course/views.py
+++ tutoring_platform/courses/course/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Course
+from .serializers import CourseSerializer
+
+class CourseListView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+"""These views use Django Rest Framework's generic views to 
+    handle listing, creating, retrieving, updating, and 
+    deleting courses."""
