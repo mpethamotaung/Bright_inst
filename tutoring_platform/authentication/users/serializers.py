2804500482c079aefec92df31521bfa20a3407d7
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 08:17:48 +0200

    in authentication/users create serializers.py to handle user data

--- tutoring_platform/authentication/users/serializers.py
+++ tutoring_platform/authentication/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUser
 
+#create userserializer class to convert user data to and from JSON format, create new users with hashed passwords
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
