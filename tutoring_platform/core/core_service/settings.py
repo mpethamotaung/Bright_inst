21fbdd4789139921373373ea44f01c616a481875
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 18:02:55 +0200

    add home app to sub main project (core_services) settings.py file

--- tutoring_platform/core/core_service/settings.py
+++ tutoring_platform/core/core_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home", #This adds home app in core_service project
 ]
 
 MIDDLEWARE = [
