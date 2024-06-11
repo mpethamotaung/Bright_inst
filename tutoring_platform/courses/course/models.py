b815ad22d5a3125a3957850a57b8e7fcd9f9a827
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 18:55:35 +0200

    tutoring_platform/courses/course/models.py this model represents the course class with the minimum required fields (title, description, tutor, created_at).

--- tutoring_platform/courses/course/models.py
+++ tutoring_platform/courses/course/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    tutor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
