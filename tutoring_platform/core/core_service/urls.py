6682448316140a93b552e31ffafd1c4936d6981c
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 18:12:57 +0200

    tutoring_platform/core/core_service/urls.py include home app path to main URL config of core_servicec project

--- tutoring_platform/core/core_service/urls.py
+++ tutoring_platform/core/core_service/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')), #includes URLs of home app
 ]
+
+"""path('', include('home.urls')),:
+    includes URLs of the home app in the main URL configuration of
+     the 'core_service' project. """
