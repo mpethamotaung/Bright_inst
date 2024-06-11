3b660b479bd8831a02eb22e14ed484a61428e4ea
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-11 08:22:58 +0200

    define url path to access the view

--- tutoring_platform/authentication/users/urls.py
+++ tutoring_platform/authentication/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import UserCreate
 
+#Define url path to access the view
 urlpatterns = [
     path('register/', UserCreate.as_view(), name='user-register'),
 ]
