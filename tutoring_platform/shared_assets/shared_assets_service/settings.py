ec58e4f8c3de8e59d69f25e2ef412ce1b2953431
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-12 10:28:36 +0200

    shared_assets_service config to route path to assets app

--- tutoring_platform/shared_assets/shared_assets_service/settings.py
+++ tutoring_platform/shared_assets/shared_assets_service/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "shared_assets_service.urls"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'assets', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,10 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
+    os.path.join(BASE_DIR, 'assets', 'static'),  # Update the path
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
