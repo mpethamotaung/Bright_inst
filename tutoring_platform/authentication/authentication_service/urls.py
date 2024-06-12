d036182b2bcbac0300c6f76953e5f2605aa5ef78
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-12 17:09:13 +0200

    update

--- tutoring_platform/authentication/authentication_service/urls.py
+++ tutoring_platform/authentication/authentication_service/urls.py
@@ -18,10 +18,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/users/', include('users.urls')), #include users app URLs
+    path('api/auth/register/', UserRegistrationView.as_view(), name='user-registration'),
+    path('api/auth/login/', UserLoginView.as_view(), name='user-login'),
 ]
 
 
