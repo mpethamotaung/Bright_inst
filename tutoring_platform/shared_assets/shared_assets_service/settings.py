3d75028cc03da40d053201bc4eba1e690d838f69
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-12 10:21:20 +0200

    update shared_assets_service, database name

--- tutoring_platform/shared_assets/shared_assets_service/settings.py
+++ tutoring_platform/shared_assets/shared_assets_service/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "shared_assets_service.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),  # Use os.path.join() to construct the file path
     }
 }
 
