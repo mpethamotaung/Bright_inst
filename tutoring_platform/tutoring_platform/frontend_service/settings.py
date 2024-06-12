aff14aab70a9de69acfe35a50844bdd1955e99e0
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-12 08:12:51 +0200

    frontend_service/settings.py add frontend app to main frontend_service

--- tutoring_platform/tutoring_platform/frontend_service/settings.py
+++ tutoring_platform/tutoring_platform/frontend_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "frontend", # add created app to main frontend_service app
 ]
 
 MIDDLEWARE = [
