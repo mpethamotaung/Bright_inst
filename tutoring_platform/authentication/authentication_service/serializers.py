2d455f30f0684cd31e6f0d85950d73182893c006
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-12 12:52:51 +0200

    implement the necessary serializers for authentication

---
+++ tutoring_platform/authentication/authentication_service/serializers.py
@@ -0,0 +1,28 @@
+#authentication/authentication_service/serializers.py
+
+#necessary module imports
+from rest_framework import serializers
+from users.models import CustomUser
+from django.contrib.auth import authenticate
+
+#User serailizer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+#User login serializer
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
