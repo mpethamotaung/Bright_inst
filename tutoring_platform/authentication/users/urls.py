1aa914230c234768be91d805f34712f13e9fdf81
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-14 11:45:58 +0200

    update... user reg api and user login api

--- tutoring_platform/authentication/users/urls.py
+++ tutoring_platform/authentication/users/urls.py
@@ -1,8 +1,9 @@
 # users/urls.py
 
 from django.urls import path
-from .views import UserCreate
+from .views import UserCreate, UserLoginView
 
 urlpatterns = [
-    path('register/', UserCreate.as_view(), name='user-register'),
+    path('register/', UserCreate.as_view(), name='user-register'), #User registration
+    path('login/', UserLoginView.as_view(), name='user-login'), #User login
 ]
