8f092e08e59acab2147dd263246da81cf0750f12
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-14 19:26:17 +0200

    Roll back to previous version of user urls.py and views.py

--- tutoring_platform/authentication/users/urls.py
+++ tutoring_platform/authentication/users/urls.py
@@ -1,12 +1,8 @@
-# authentication/authentication_service/users/urls.py
-
+# users/urls.py
 from django.urls import path
-from .views import UserCreate, UserLogin, UserDetails, UserLogout
-from django.views.generic import TemplateView
+from .views import UserCreate, UserDetailView
 
 urlpatterns = [
-    path('register/', TemplateView.as_view(template_name='register.html'), name='register'),
-    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
-    path('user-details/', UserDetails.as_view(), name='user-details'), 
-    path('logout/', TemplateView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', UserCreate.as_view(), name='user-register'),
+    path('me/', UserDetailView.as_view(), name='user-detail'), #route URL path
 ]
--- tutoring_platform/authentication/users/views.py
+++ tutoring_platform/authentication/users/views.py
@@ -1,5 +1,3 @@
-# authentication/authentication_service/users/views.py
-
 from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserSerializer
@@ -9,57 +7,18 @@ from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveUpdateAPIView
 
-#User templates navigation imports
-from django.shortcuts import render, redirect
-from django.views import View
-from django.contrib.auth import login, logout, authenticate
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.tokens import RefreshToken
-
 #Add an endpoint to retrive user details.
 #This allows authenticated users to get their profile information
-class UserCreate(View):
-    def get(self, request):
-        return render(request, 'register.html')
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.POST)
-        if serializer.is_valid():
-            serializer.save()
-            return redirect('login')
-        return render(request, 'register.html', {'errors': serializer.errors})
-
-class UserLogin(View):
-    def get(self, request):
-        return render(request, 'login.html')
-
-    def post(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            refresh = RefreshToken.for_user(user)
-            request.session['refresh_token'] = str(refresh)
-            return redirect('user-details')
-        return render(request, 'login.html', {'error': 'Invalid credentials'})
-
-class UserDetails(APIView):
+class UserDetailView(RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
 
-    def get(self, request):
-        user = request.user
-        return render(request, 'user_details.html', {'user': user})
-
-class UserLogout(View):
-    def get(self, request):
-        return render(request, 'logout.html')
-
-    def post(self, request):
-        refresh_token = request.session.get('refresh_token')
-        token = RefreshToken(refresh_token)
-        token.blacklist()
-        logout(request)
-        return redirect('login')
+    def get_object(self):
+        return self.request.user
+    
+#User class to allow users to access this endpoint
+class UserCreate(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
