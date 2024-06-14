3d638b572d56cf02f65c257ef6c5894f4382193f
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-14 19:12:17 +0200

    update user details path and logout path. Update views to handle html templates

--- tutoring_platform/authentication/users/urls.py
+++ tutoring_platform/authentication/users/urls.py
@@ -1,8 +1,12 @@
-# users/urls.py
+# authentication/authentication_service/users/urls.py
+
 from django.urls import path
-from .views import UserCreate, UserDetailView
+from .views import UserCreate, UserLogin, UserDetails, UserLogout
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('register/', UserCreate.as_view(), name='user-register'),
-    path('me/', UserDetailView.as_view(), name='user-detail'), #route URL path
+    path('register/', TemplateView.as_view(template_name='register.html'), name='register'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('user-details/', UserDetails.as_view(), name='user-details'), 
+    path('logout/', TemplateView.as_view(template_name='logout.html'), name='logout'),
 ]
