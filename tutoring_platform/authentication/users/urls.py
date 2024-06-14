2e779f43e53a51af1c9caf92467c7f0585ad8ee3
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-14 17:17:07 +0200

    Add endpoint to retrieve user details. allows authenticated users to get their profiel information.

--- tutoring_platform/authentication/users/urls.py
+++ tutoring_platform/authentication/users/urls.py
@@ -1,7 +1,8 @@
 # users/urls.py
 from django.urls import path
-from .views import UserCreate
+from .views import UserCreate, UserDetailView
 
 urlpatterns = [
     path('register/', UserCreate.as_view(), name='user-register'),
+    path('me/', UserDetailView.as_view(), name='user-detail'), #route URL path
 ]
--- tutoring_platform/authentication/users/views.py
+++ tutoring_platform/authentication/users/views.py
@@ -3,8 +3,20 @@ from .models import CustomUser
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 
+#User Profile imports
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import RetrieveUpdateAPIView
 
-#initiate user class to allow users to access this endpoint
+#Add an endpoint to retrive user details.
+#This allows authenticated users to get their profile information
+class UserDetailView(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+    
+#User class to allow users to access this endpoint
 class UserCreate(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
