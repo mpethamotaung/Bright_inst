d7cc522e3245b1bfdc6790455744391e7c87ebb4
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-20 14:19:39 +0200

    add extra fields in user model

--- tutoring_platform/authentication/users/models.py
+++ tutoring_platform/authentication/users/models.py
@@ -1,5 +1,13 @@
-from django.contrib.auth.models import AbstractUser
+# users/models.py
 
-class CustomUser(AbstractUser):
-    # Add any additional fields here
-    pass
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_tutor = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
