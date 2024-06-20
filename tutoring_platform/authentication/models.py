b3a77c6ed85db59d61939163b7d03a07886528ce
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-20 14:52:48 +0200

    authentication rollback and recreate

--- tutoring_platform/authentication/models.py
+++
@@ -1,9 +0,0 @@
-# users/models.py
-
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    is_tutor = models.BooleanField(default=False)
-    is_student = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
