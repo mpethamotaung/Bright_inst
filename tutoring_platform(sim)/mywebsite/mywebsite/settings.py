b6ead8a0049f3fddbf464f09daf8c1862b61d9e0
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-21 11:21:37 +0200

    configure the static files settings to point to assets directory

--- tutoring_platform(sim)/mywebsite/mywebsite/settings.py
+++ tutoring_platform(sim)/mywebsite/mywebsite/settings.py
@@ -118,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#configure the static files settings to point to assets directory
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
